5b9c5e360c3e00b6fe6f029a4fe481f9f43ec571
weinsh88 <elliottweinshenker@gmail.com>
2022-12-13 12:14:20 -0500

    fixed format and add linting

--- src/d3/d3.tsx
+++ src/d3/d3.tsx
@@ -7,23 +7,21 @@ const lines = input.split('\n');
 let sum = 0;
 
 const letterToIndex = (letter: string): number => {
-	const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	return alphabet.indexOf(letter) + 1;
+  const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return alphabet.indexOf(letter) + 1;
 };
 
 lines.forEach((line) => {
-	let sz = line.length / 2;
-	const c1 = new Set(
-		Array.from(line.slice(0, sz)).map((l) => letterToIndex(l))
-	);
-	const c2 = Array.from(line.slice(sz)).map((l) => letterToIndex(l));
+  let sz = line.length / 2;
+  const c1 = new Set(Array.from(line.slice(0, sz)).map((l) => letterToIndex(l)));
+  const c2 = Array.from(line.slice(sz)).map((l) => letterToIndex(l));
 
-	let val = 0;
-	c2.forEach((num) => {
-		if (c1.has(num)) val = num;
-	});
+  let val = 0;
+  c2.forEach((num) => {
+    if (c1.has(num)) val = num;
+  });
 
-	sum += val;
+  sum += val;
 });
 
 console.log(sum);
